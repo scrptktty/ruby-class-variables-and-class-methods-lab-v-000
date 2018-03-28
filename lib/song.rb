e470d3215b92f019a6e729ebb0aac0a65a54c00e
Kachel Wittig <kjwittig@gmail.com>
2018-03-28 18:37:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,12 +30,12 @@ class Song
 
 # array.reduce(initial_val) { |previous_val, x | code }
   def self.genre_count
-    @@genres.reduce({}) { |genre_count_hash, genre|
+    @@genres.reduce({}) do |genre_count_hash, genre|
       genre_count_hash[genre] ||= 0
       # genre_count_hash[genre] = genre_count_hash[genre] + 1
       genre_count_hash[genre] += 1
       # genre_count_hash[genre]++
-    }
+    end
   end
 
  def self.artist_count
